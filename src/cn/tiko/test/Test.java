00744c96d3848cd7682f33673daca920109ca204
chordstrife <18296546505@163.com>
2020-05-27 14:17:42 +0800

    2

---
+++ src/cn/tiko/test/Test.java
@@ -0,0 +1,10 @@
+package cn.tiko.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("git TEST!!");
+    }
+    public void test(){
+        int i = 0;
+    }
+}
